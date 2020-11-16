c4a672a9fdf262a76b121e6e2662280346d485a2
Dmitry <dima153@gmail.com>
2020-11-16 22:29:31 +0500

    task 1

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -8,8 +8,29 @@ namespace HW2
 {
 	class Program
 	{
+		/// <summary>
+		/// Возврат минимального из трех
+		/// </summary>
+		/// <param name="a">аргумент 1</param>
+		/// <param name="b">аргумент 2</param>
+		/// <param name="c">аргумент 3</param>
+		/// <returns></returns>
+		static int MINOF3(int a, int b, int c)
+		{
+			return a < b && a < c ? a : b < c ? b : c;
+
+		}
 		static void Main(string[] args)
 		{
+
+			#region task1
+			Console.WriteLine(MINOF3(1, 3, 5));
+
+
+
+
+			#endregion
+			Console.ReadLine();
 		}
 	}
 }
