1ffc6432a861d1e685bb9608e2f1e9f939253cb3
Dmitry <dima153@gmail.com>
2020-11-21 18:38:20 +0500

    HW2 task2

---
+++ HW3/Complex.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+	/// <summary>
+	/// Комлпексные числа через структуру
+	/// </summary>
+	struct Complex
+	{
+		public double im;
+		public double re;
+
+		public Complex Plus(Complex x)
+		{
+			Complex y;
+			y.im = im + x.im;
+			y.re = re + x.re;
+			return y;
+
+		}
+
+		public Complex Minus(Complex x)
+		{
+			Complex y;
+			y.im = im - x.im;
+			y.re = re - x.re;
+			return y;
+		}
+
+		public Complex Multi(Complex x)
+		{
+			Complex y;
+			y.im = (im * x.im - re * x.re);
+			y.re = (im * x.re + re * x.im);
+			return y;
+		}
+
+
+		public Complex Test()
+		{
+			Complex Z;
+			Z.im = 2;
+			Z.re = 3;
+			return Z;
+
+		}
+
+		public string ToString()
+		{ return re + " + " + im + " i "; }
+
+
+	}
+
+	class COMPLEX
+	{
+		double im;
+		double re;
+
+		public COMPLEX()
+		{
+			im = 0;
+			re = 0;
+		}
+
+		public COMPLEX(double im, double re)
+		{
+			im = im;
+			re = re;
+		}
+
+
+
+		public COMPLEX Plus(COMPLEX x)
+		{
+			COMPLEX y = new COMPLEX();
+			y.im = im + x.im;
+			y.re = re + x.re;
+			return y;
+
+		}
+
+		public COMPLEX Minus(COMPLEX x)
+		{
+			COMPLEX y = new COMPLEX();
+			y.im = im - x.im;
+			y.re = re - x.re;
+			return y;
+		}
+
+		public COMPLEX Multi(COMPLEX x)
+		{
+			COMPLEX y = new COMPLEX();
+			y.im = (im * x.im - re * x.re);
+			y.re = (im * x.re + re * x.im);
+			return y;
+		}
+
+       public double Im
+		{
+			get { return im; }
+			set { if (value > 0) im = value; }
+		}
+
+		public double Re
+		{
+			get { return re; }
+			set { re = value; }
+		}
+
+		public string ToString()
+		{ return re + " + " + im + " i "; }
+
+
+	}
+
+
+}
---
+++ HW3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Complex complex1;
+			complex1.re = 1;
+			complex1.im = 1;
+			Complex complex2;
+			complex2.re = 2;
+			complex2.im = 2;
+
+			COMPLEX com1 = new COMPLEX();
+			com1.Re = 1;
+			com1.Im = 1;
+
+			COMPLEX com2 = new COMPLEX();
+			com2.Re = 2;
+			com2.Im = 2;
+
+
+
+			Complex result2 = complex1.Plus(complex2);
+			Console.WriteLine( $"Сложение:{result2.ToString()} \n Перемножение: {complex1.Multi(complex2).ToString()} \n  ");
+
+			Console.WriteLine($"Сложение:{com1.Plus(com2).ToString()} \n Перемножение: {com1.Multi(com2).ToString()} \n  ");
+
+
+			Console.ReadKey();
+		}
+	}
+}
