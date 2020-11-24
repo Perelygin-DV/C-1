e3cad0fe2ffb2fe229bccd6eabd148976100d56f
Dmitry <dima153@gmail.com>
2020-11-24 23:44:07 +0500

    HW4 task 1

---
+++ HW4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Finder fn = new Finder();
+			fn.View();
+			fn.Number();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ HW4/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+	class Finder
+	{
+		Random rnd = new Random();
+		
+		int[] m = new int[20];
+
+		public Finder()
+		{
+
+			for (int i = 0; i < m.Length; i++)
+			{
+				m[i] = rnd.Next(-10, 10);
+			}
+
+		}
+
+		public void View()
+		{
+			for (int i = 0; i < m.Length; i++)
+			{
+				Console.Write($"{m[i]}  ");
+
+			}
+		}
+
+		public void Number ()
+		{
+			int count = 0;
+			for (int i = 0; i < m.Length-1; i++)
+			{
+				if (m[i] % 3 == 0 | m[i + 1] % 3 == 0) count++;
+			}
+			Console.WriteLine($"\n число : {count}");
+		}
+
+
+
+		
+
+
+	}
+}
